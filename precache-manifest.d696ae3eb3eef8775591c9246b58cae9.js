6a2b76399bd2114e2d9d502d28e9abcca7c2b955
spiridon209 <sivand888@gmail.com>
2020-08-23 19:13:44 +0500

    Updates

---
+++ precache-manifest.d696ae3eb3eef8775591c9246b58cae9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "844ad0919d744bc06357c1bd27d6b893",
+    "url": "/Rocket-Test-Task/index.html"
+  },
+  {
+    "revision": "a1d9a57041ec199b6884",
+    "url": "/Rocket-Test-Task/static/css/main.636d4c31.chunk.css"
+  },
+  {
+    "revision": "8526ab445d314e270bba",
+    "url": "/Rocket-Test-Task/static/js/2.24823e24.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Rocket-Test-Task/static/js/2.24823e24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1d9a57041ec199b6884",
+    "url": "/Rocket-Test-Task/static/js/main.5135dab6.chunk.js"
+  },
+  {
+    "revision": "6adc1d30926d6a652dde",
+    "url": "/Rocket-Test-Task/static/js/runtime-main.11e0eac4.js"
+  }
+]);
